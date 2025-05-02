9b133a1fc7b53503e5a194c17385f92b4d7b341a
Beirun <orillobernardjay@gmail.com>
2025-05-02 17:29:36 +0800

    update

---
+++ Data/Object/GuestBooking.cs
@@ -0,0 +1,13 @@
+﻿namespace Oasis.Data.Object
+{
+    public class GuestBooking
+    {
+        public int rsv_id { get; set; }
+        public int room_no { get; set; }
+        public string type_category { get; set; }
+        public DateOnly rsv_checkin { get; set; }
+        public DateOnly rsv_checkout { get; set; }
+        public string rsv_status { get; set; }
+        public double payment_amount { get; set; }
+    }
+}
