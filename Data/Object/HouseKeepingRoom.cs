0451f81bf1e77332b771bc5dd4c5898f1249b6fd
Beirun <orillobernardjay@gmail.com>
2025-05-05 18:37:36 +0800

    housekeeper update

--- Data/Object/HouseKeepingRoom.cs
+++ Data/Object/HouseKeepingRoom.cs
@@ -4,6 +4,8 @@
     {
         public int room_id { get; set; }
         public int staff_id { get; set; }
+        public string? user_fname { get; set; }
+        public string? user_lname { get; set; }
         public int room_no { get; set; }
         public string room_status { get; set; }
         public DateTime? housekeeping_starttime { get; set; }
